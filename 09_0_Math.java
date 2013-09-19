0ee9cbbedeb45dadba1aca13bb53ff170cc7bc2e
huihan <huihancarl@gmail.com>
2013-09-19 11:22:14 +0800

    Create 09_0_Math.java

---
+++ 09_0_Math.java
@@ -0,0 +1,9 @@
+http://www.careercup.com/question?id=10072145
+three points are randomly chosen on a circle.what the probability that 
+1.triangle formed is right angled triangle. 
+2.triangle formed is acute angled triangle. 
+3.triangle formed is obtuse angled triangle.
+
+
+
+I think it is 75%
